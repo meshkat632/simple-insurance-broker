a053e2d52533bd3eb3c5155841f1802a90be5307
Anwer Meshkatul (INST/ECS4) <Meshkatul.Anwer@bosch-si.com>
2020-05-30 21:44:17 +0200

    terrafom eks cluster

---
+++ infrastructure/terraform-src/providers.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+data "aws_region" "current" {
+}
+
+data "aws_availability_zones" "available" {
+}
+
+provider "http" {
+}
+
---
+++ infrastructure/terraform-src/vars.tf
@@ -0,0 +1,9 @@
+variable "cluster-name" {
+  default = "simple-insurence-broker-eks-cluster"
+  type    = string
+}
+
+variable "vpc-name" {
+  default = "simple-insurence-broker-eks-cluster-vpc"
+  type    = string
+}
