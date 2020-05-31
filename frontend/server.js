f60c15977a82e96cacf44aa2f73b8de09bd4a8fd
Anwer Meshkatul (INST/ECS4) <Meshkatul.Anwer@bosch-si.com>
2020-05-31 22:24:47 +0200

    deploy frontend into minikube

--- frontend/server.js
+++ frontend/server.js
@@ -1,12 +1,100 @@
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
+
 const express = require('express')
 const app = express()
-app.set('view-engine', 'ejs')
-app.use(express.static('resources'));
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const bcrypt = require('bcrypt')
+const methodOverride = require('method-override')
+
 global.__basedir = __dirname;
 var path = __basedir + '/views/';
+
+const initializePassport = require('./passport-config')
+initializePassport(
+    passport,
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+)
+
+const users = []
+app.set('view-engine', 'ejs')
+app.use(express.static('resources'));
+app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+
+app.get('/', checkAuthenticated, (req, res) => {
+    res.render('contracts.ejs', {name: req.user.name})
+
+})
+app.delete('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/login')
+})
+
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
+
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
+})
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect('/login')
+    } catch (e) {
+        console.error(e)
+        res.redirect('/register')
+    }
+    console.log(users)
+})
+app.get('/api/customers/all', checkAuthenticated, (req, res) =>{
+    console.log("--->Get All Customers: \n" + JSON.stringify(customers, null, 4));
+    res.send(customers);
+});
+app.use("*", (req,res) => {
+    res.sendFile(path + "404.html");
+});
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next()
+    }
+    res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/')
+    }
+    next()
+}
+
 // Fetch all Customers
 var customers = [
     {
@@ -56,20 +144,5 @@ var customers = [
     }
 ]
 
-exports.getAll = (req, res) => {
-    console.log("--->Get All Customers: \n" + JSON.stringify(customers, null, 4));
-    res.send(customers);
-};
 
-
-app.get('/', (req, res) => {
-    res.render('index.ejs', {name: "WTF"})
-})
-app.get('/api/customers/all', (req, res) =>{
-    console.log("--->Get All Customers: \n" + JSON.stringify(customers, null, 4));
-    res.send(customers);
-});
-app.use("*", (req,res) => {
-    res.sendFile(path + "404.html");
-});
 app.listen(3001)
